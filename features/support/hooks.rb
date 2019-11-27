7aebfe8f4e46b7b5458b4b961b837a09b5a36d7b
02731893109 <johnatan.silva@poupex.com.br>
2019-11-27 11:08:40 -0300

    refatorando cucumber.yaml e .gitgnore

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -10,7 +10,7 @@ After do |scenario|
     nome_cenario = nome_cenario.gsub(' ', '_').downcase!
     screenshot = page.save_screenshot("log/#{nome_cenario}.png")
     shot = Base64.encode64(File.open(screenshot, "rb").read)
-    embed(shot, "image/png", "Confira a evidência")
+    embed(shot, "image/png")
     @LoginPage = LoginPage.new
     @LoginPage.faz_logoff_sistemas
   end
